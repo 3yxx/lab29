72b9d555148f680715c87e4b79f7f3ece3108f73
3yxx <127534673+3yxx@users.noreply.github.com>
2025-03-03 10:56:23 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?
 session_start();
-define("APP_ID",'53545980');
-define("APP_SECRET",'1Maasuv2vHnJO3AOaKFC');
-define("REDIRECT_URI",'http://Laba29/auth.php');
+define("APP_ID",'53176784');
+define("APP_SECRET",'tOLYOuasPs8CXs2GE9ZL');
+define("REDIRECT_URI",'http://3yxx.github.io/auth.php');
 define("URL_ACCESS_TOKEN",'https://oauth.vk.com/access_token');
 define("URL_AUTH",'http://oauth.vk.com/authorize');
 define("URL_GET_USER",'https://api.vk.com/method/users.get');
